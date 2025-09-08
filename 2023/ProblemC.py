a3999092b377110effda090f55b161d1cbcec244
Farid-Abdullah <60304642@udst.edu.qa>
2025-09-08 18:56:13 +0300

    till Problem C

---
+++ 2023/ProblemC.py
@@ -0,0 +1,11 @@
+
+
+# Hotel Rooms
+
+
+dictionary = {1:"Mohamed",2:"Ahmed",3:"Mohsen"}
+def occupant(key):
+    return dictionary[key]
+
+# that's what i understand from the question.
+# am i missing something?
