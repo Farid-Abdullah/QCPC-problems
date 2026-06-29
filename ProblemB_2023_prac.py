651561a7d8e9f27c8fac9d35d7a24e9e57579b78
Farid <60304642@udst.edu.qa>
2026-06-29 22:49:11 +0300

    satisfactory solution 'sol1'

--- ProblemB_2023_prac.py
+++ ProblemB_2023_prac.py
@@ -22,10 +22,7 @@
     ouput: 30
 '''
 def sol1():
-    '''
-    Could be improved, for example, the loop in line 47, that loop could make the time complexity O(n2), I think we can avoid the loop using offset variable.
-    '''
-# hardcoding case 3:
+
     n = 11 # 
     arr = [-5, 0, 4, 6, 3, 8, 2, 1, 0, 9]
     #arr = [0,1,0,2,0,3,0,4,0,5]
@@ -40,25 +37,21 @@ def sol1():
     else:
         base = 0
 
-    def split_and_count(arr, mVal, mi):
-        decBy = mVal+base
+    def split_and_count(arr, mVal, mi,offset=0): # Need for offset variable is that we won't need a loop to decrement the subarray !! saving big chunk on time complexity!!!
+        decBy = mVal+base-offset 
         state["answer"]+= decBy
-        
-        for i in range(len(arr)):
-            arr[i]-=decBy
-            
-
         sub1 = arr[mi+1:]
+
         if sub1 != []:
             mVal1= min(sub1)
             mi1= sub1.index(mVal1)
-            split_and_count(arr[mi+1:],mVal1, mi1)
+            split_and_count(arr[mi+1:],mVal1, mi1,decBy+offset)
     
         sub2 = arr[:mi]
         if sub2 !=[]:
             mVal2= min(sub2)
             mi2= sub2.index(mVal2)
-            split_and_count(arr[:mi],mVal2, mi2)
+            split_and_count(arr[:mi],mVal2, mi2,decBy+offset)
 
     split_and_count(arr, mVal, mi)
     print(state["answer"])
